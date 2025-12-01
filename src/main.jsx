f7b1e97e9bf7878a14ef417e5173f337e4b5d27f
Log0ns <55673686+Log0ns@users.noreply.github.com>
2025-12-01 11:58:44 -0600

    Reorder index.css import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import OrgBook from "./OrgBook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
